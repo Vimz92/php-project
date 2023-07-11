bdc6d00325358a634cd6fd07239a89b9083af995
vimal vinayraj <vimalvinayraj@gmail.com>
2023-07-11 11:02:50 +0400

    connected-with-database

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,10 +8,12 @@
     <link rel="stylesheet" href="../style.css">
     <!-- bootstrap css link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+     <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <style>
 .admin_image {
-    width: 80px;
+    width: 100px;
     object-fit: contain;
 }
 </style>
@@ -39,26 +41,50 @@
 
     <!-- third child -->
     <div class="row">
-        <div class="col-md-12 bg-secondary">
-            <div>
+        <div class="col-md-12 bg-secondary d-flex">
+            <div class="p-3">
                 <a href="#"><img src="../images/banana.jpg" alt="" class="admin_image">
                 <p class="text-light text-center">Admin Name</p>
-             </div>
+             </div> 
              <div class="button text-center"> 
                    <button><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
-                   <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                   <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
-                   <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                   <button><a href="index.php?insert_brands" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
                    <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
-                   <button><a href="" class="nav-link text-light bg-info my-1"></a>Logout</button>
+                   <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
              </div>
         </div>
+    </div>  
 
+    <!-- fourth child -->
+     <div class="container">
+    <?php
+    if(isset($_GET['insert_category'])){
+        include('insert_categories.php');
+    }
+
+    if(isset($_GET['insert_brands'])){
+        include('insert_brands.php');
+    }
+    ?>
+
+
+
+     </div>
+
+
+
+
+    <!-- footer -->
+    <div class="bg-info text-center">
+        <p> All rights reserved @Vimal Vinayraj </p>
     </div>
+
 
    </div>
 
